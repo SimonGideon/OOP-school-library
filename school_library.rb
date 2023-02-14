8e9d3eac827e131fbca996d81ba224b0cb86443b
SimonGideon <simongideon918@gmail.com>
2023-02-14 20:15:46 +0300

    Solve requested changes

--- school_library.rb
+++ school_library.rb
@@ -10,19 +10,11 @@ class Person
 
   attr_reader :age
 
-  def add_name(value)
-    @name = value
-  end
-
-  def add_age(age)
-    @age = age
-  end
-
   Private def of_age
     @age >= 18
   end
   def can_use_services
-    return unless @age < 18 || parent_permission
+    return unless @age >= 18 || parent_permission
 
     true
   end
