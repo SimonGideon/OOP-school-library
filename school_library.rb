be106781d271e63e4e5e9d5ad5b2ca189b5c244a
SimonGideon <simongideon918@gmail.com>
2023-02-15 11:48:02 +0300

    Decorators

--- school_library.rb
+++ school_library.rb
@@ -1,5 +1,7 @@
-class Person
+require './module/nameable'
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
@@ -11,12 +13,43 @@ class Person
 
   attr_accessor :age
 
-  Private def of_age
+  private
+  
+  def of_age
     @age >= 18
   end
   def can_use_services
     return unless @age >= 18 || parent_permission
 
     true
+  end
+
+  def correct_name
+    @name
+  end
+end
+
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    nameable
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    nameable.upcase!
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    nameable[0..9]
   end
 end
